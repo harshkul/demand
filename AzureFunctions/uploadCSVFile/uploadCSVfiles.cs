cf01e3aebdbd44fae8b3b9663db5be44cf3b1b4a
harshkul <kulshreshtha.harsh5@gmail.com>
2022-04-21 18:25:53 +0530

    csv upload
    csv upload

--- AzureFunctions/uploadCSVFile/uploadCSVfiles.cs
+++ AzureFunctions/uploadCSVFile/uploadCSVfiles.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Azure.Storage.Blobs;
 
 namespace PaymentServices
 {
@@ -14,21 +16,33 @@ namespace PaymentServices
     {
         [FunctionName("uploadCSVfiles")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Blob("elcsvfiles", FileAccess.ReadWrite,Connection="AzureWebJobsStorage")] BlobContainerClient outputBlob,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"File Upload request recevied at {DateTime.Now.ToString()}");
 
-            string name = req.Query["name"];
+            string fileName = String.Empty;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            for (int i = 0; i < req.Form.Files.Count; i++)
+            {
+                fileName = req.Query["fileName"];
+                var file = req.Form.Files[i];
+                var fileNameL = file.FileName;
+                Stream stream = file.OpenReadStream();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                outputBlob.UploadBlobAsync(fileNameL, stream).Wait();
 
+            }
+
+
+            string responseMessage = string.IsNullOrEmpty(fileName) && req.Form.Files.Count == 0
+                ? "This HTTP triggered function executed successfully. but there is no file to upload."
+                : $"File, {fileName} is uploaded successfully.";
+
+
+            log.LogInformation(responseMessage);
             return new OkObjectResult(responseMessage);
         }
     }
